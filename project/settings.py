494c3fa5920fabf2a3573b826b19cb8ecf91e11c
MauWebs <mauriciovidalwebs@gmail.com>
2023-11-15 14:18:42 -0300

    cors-mauwebs

--- project/settings.py
+++ project/settings.py
@@ -36,13 +36,13 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://mauwebs.github.io/React-Pc",
+    "https://mauwebs.github.io",
 ]
 
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
-    "https://mauwebs.github.io/React-Pc",
+    "https://mauwebs.github.io",
 ]
 
 CORS_ALLOW_METHODS = (
